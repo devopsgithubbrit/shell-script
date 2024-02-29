a84c843940bf002c2032f6b0768b72f1faeeee02
Vinuthna <vinnuch2408@gmail.com>
2024-02-29 11:42:29 -0800

    added 2nd script

--- 10-functions.sh
+++ 10-functions.sh
@@ -3,21 +3,27 @@
 ID=$(id -u)
 
 VALIDATE(){
-if [ $? -ne 0 ]
+if [ $1 -ne 0 ]
 then 
-    echo "ERROR:: Installing is failed"
+    echo "ERROR:: $2 is failed"
     exit 1
 else
-    echo "Installing  is SUCCESS"
+    echo "$2 is SUCCESS"
 fi
 }
-
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+    echo "You are root user"
+fi
 
 yum install mysql -y
 
-VALIDATE 
+VALIDATE $? "Mysql"
 
 yum install git -y
 
-VALIDATE 
+VALIDATE $? "GIT"
 
