301b84c8e78e0d6ce0fea11ffb0f64714385a20c
Vinuthna <vinnuch2408@gmail.com>
2024-03-02 18:12:04 -0800

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -28,3 +28,11 @@ yum install git -y
 
 VALIDATE $? "GIT"
 
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+    echo "You are root user"
+fi
+
---
+++ without-exit-status.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+ls -ltr
+
+touch example.txt
+
+echo "Before wrong command"
+
+lsffff
+
+
+echo "After wrong command"
+
+cd /tmp
+
+cd /home/centos
+
+
