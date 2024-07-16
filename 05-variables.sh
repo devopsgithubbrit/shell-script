762dd86ae5c0abb44278e0b3c5966c1ead5ca684
devopsgithubbrit <vinnuch2408@gmail.com>
2024-07-16 06:12:56 -0700

    1st-script

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enetr your username ::"
+raed -s USERNAME
+
+echo "Please enter your password ::"
+read -s PASSWORD
+
+echo "Username is  = $USERNAME , PASSOWRD is = $PASSWORD"
