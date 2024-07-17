b6b571b8d2cd0000f92bec3360502b1870f4dde8
devopsgithubbrit <vinnuch2408@gmail.com>
2024-07-17 00:12:23 -0700

    1st-script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -2,30 +2,30 @@
 
 ID=$(id -u)
 
-if [$ID -ne 0]
+if [ $ID -ne 0 ]
 then
-    echo "ERROR :: Run the script with root user"
-    exit 1
-else 
-    echo "You have sudo access"
-fi
-
-yum install myyyyyysql -y
-
-if [$? -ne 0]
-then 
-    echo "ERROR:: Installing MYSQL is FAILED"
-    exit 1
-else 
-    echo "Installing MYSQL is SUCCESS"
+    echo "ERROR:: Please run this script with root access"
+    exit 1 
+else
+    echo "You are root user"
 fi 
+
+yum install myysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing MySQL is failed"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
 
 yum install git -y
 
-if [$? -ne 0]
-then 
-    echo "ERROR:: Installing GIT is FAILED"
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing GIT is failed"
     exit 1
-else 
+else
     echo "Installing GIT is SUCCESS"
-fi 
+fi
