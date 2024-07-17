85fbbed37eaf0dfb04e48c45236084d1f8579b1d
devopsgithubbrit <vinnuch2408@gmail.com>
2024-07-17 00:17:42 -0700

    1st-script

--- 10-functions.sh
+++ 10-functions.sh
@@ -3,16 +3,16 @@
 ID=$(id -u)
 
 VALIDATE() {
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
 then 
-    echo "ERROR:: Installing $2 is FAILED"
+    echo "ERROR::  $2 is FAILED"
     exit 1
 else 
-    echo "Installing $2 is SUCCESS"
+    echo " $2 is SUCCESS"
 fi 
 }
 
-if [$ID -ne 0]
+if [ $ID -ne 0 ]
 then
     echo "ERROR:: Run the script with root access"
     exit 1
